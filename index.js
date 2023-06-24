344b70e681e49d1c218ba103a33e5be9de66c366
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-06-24 09:37:37 +0300

    complete 0- objects-lab js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const = employee {
+    name: "Emmanuel",
+    streetAddress: "254 nrb"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+     // Return a new object with the updated key-value pair using the spread operator 
+
+return{
+    ...employee,//spread operator
+    [key]: value
+};
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+
+ function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
